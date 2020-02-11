a994ad1b0a79c0627a3d09f92d1a31e6716446b5
djruiz <djruiz@iu.edu>
2020-02-11 13:56:57 -0500

    2/1//20 website

---
+++ contact.php
@@ -0,0 +1,3 @@
+<?php if(isset($_POST['name'], $_POST['message'])) {
+    print_r($_POST)
+}
---
+++ thanks.php
@@ -0,0 +1,14 @@
+<?php
+/* Template Name: Thanks */
+
+/**
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+
+<h4>Testing</h4>
